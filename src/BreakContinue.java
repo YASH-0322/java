1712d082a8dda206196cbab562715b8d84b44576
yash <ychourasiya322@gmail.com>
2026-06-19 19:02:47 +0530

    Break Continue use

---
+++ src/BreakContinue.java
@@ -0,0 +1,20 @@
+public class BreakContinue {
+    public static void main(String []args){
+        for(int i=1;i<=10;i++){
+
+            //BREAK   USE
+
+
+//            if(i==5) break;{//break k matlbh stop kr dena yha 5 bhi nhi chlega
+//
+//                System.out.println(i+" hello");
+
+
+            //CONTINUE USE
+            if(i==5)continue;{//continue k matlbhai yhe sirf 5 ko skip krdega or ange badhte jyga
+                System.out.println(i+ " hellow");
+
+            }
+        }
+    }
+}
